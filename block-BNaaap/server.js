caab9e4382b72293230ce610acb7ba5062e1bb25
Venky0003 <rajv17270@gmail.com>
2023-04-12 11:50:47 +0530

    response and use postman

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,11 +3,13 @@ let http = require('http');
 let server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
+  console.log(req.method);
   res.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/Html')
   res.writeHead(200, { 'Content-Type': 'text/Html' });
   res.write('<h1>Hello</h1>');
   res.end();
 }
 
-server.listen(4444);
+server.listen(4444, () => {
+  console.log('Server is listening');
+});
